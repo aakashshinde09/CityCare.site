1862c72ebd2bbe1ddc8a3ad9d48850233d9d76c4
Akash Shinde <saakashs1999@gmail.com>
2024-07-13 14:10:40 +0530

    added a script to be used for deployment

---
+++ data/script.sh
@@ -0,0 +1,10 @@
+# Define the new values
+new_host="database-1.cru2a806a2xh.us-east-1.rds.amazonaws.com"
+new_db="citycare"
+new_user="admin"
+new_password="akash123"
+
+# Update the persistence.xml file
+sed -i "s|jdbc:mysql://localhost:3306/hospital3?createDatabaseIfNotExist=true|jdbc:mysql://${new_host}:3306/${new_db}?createDatabaseIfNotExist=true|" persistence.xml
+sed -i "s|<property name=\"javax.persistence.jdbc.user\" value=\"root\" />|<property name=\"javax.persistence.jdbc.user\" value=\"${new_user}\" />|" persistence.xml
+sed -i "s|<property name=\"javax.persistence.jdbc.password\" value=\"Akash\" />|<property name=\"javax.persistence.jdbc.password\" value=\"${new_password}\" />|" persistence.xml
