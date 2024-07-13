c0732e3c369d65000a556a1f4cba1a8a3610b8e2
Akash Shinde <saakashs1999@gmail.com>
2024-07-13 14:18:51 +0530

    updated

--- data/script.sh
+++ data/script.sh
@@ -5,6 +5,6 @@ new_user="admin"
 new_password="akash123"
 
 # Update the persistence.xml file
-sed -i "s|jdbc:mysql://localhost:3306/hospital3?createDatabaseIfNotExist=true|jdbc:mysql://${new_host}:3306/${new_db}?createDatabaseIfNotExist=true|" persistence.xml
-sed -i "s|<property name=\"javax.persistence.jdbc.user\" value=\"root\" />|<property name=\"javax.persistence.jdbc.user\" value=\"${new_user}\" />|" persistence.xml
-sed -i "s|<property name=\"javax.persistence.jdbc.password\" value=\"Akash\" />|<property name=\"javax.persistence.jdbc.password\" value=\"${new_password}\" />|" persistence.xml
+sed -i "s|jdbc:mysql://localhost:3306/hospital3?createDatabaseIfNotExist=true|jdbc:mysql://${new_host}:3306/${new_db}?createDatabaseIfNotExist=true|" /var/lib/jenkins/workspace/citycare/src/main/resources/META-INF/persistence.xml
+sed -i "s|<property name=\"javax.persistence.jdbc.user\" value=\"root\" />|<property name=\"javax.persistence.jdbc.user\" value=\"${new_user}\" />|" /var/lib/jenkins/workspace/citycare/src/main/resources/META-INF/persistence.xml
+sed -i "s|<property name=\"javax.persistence.jdbc.password\" value=\"Akash\" />|<property name=\"javax.persistence.jdbc.password\" value=\"${new_password}\" />|" /var/lib/jenkins/workspace/citycare/src/main/resources/META-INF/persistence.xml
